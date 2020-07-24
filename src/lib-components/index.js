1812c1181ec0faf8234820586ed5762ec1f19676
Jermaine <jermaine@Jermaines-Mac-mini.local>
2020-07-24 16:31:19 -0700

    Fix import

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,3 +1,3 @@
 /* eslint-disable import/prefer-default-export */
 export { default as ShopBaseComponentsSample } from './shop-base-components-sample.vue'
-export { default as AnnouncementBar } from './AnnouncementBar'
+export { default as AnnouncementBar } from './AnnouncementBar.vue'

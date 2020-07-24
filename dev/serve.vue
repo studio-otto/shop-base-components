e34655a83f559ab5afe6c8373c5d3f18e54e0719
Jermaine <jermaine@Jermaines-Mac-mini.local>
2020-07-24 12:25:41 -0700

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { ShopBaseComponentsSample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    ShopBaseComponentsSample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <shop-base-components-sample />
+  </div>
+</template>

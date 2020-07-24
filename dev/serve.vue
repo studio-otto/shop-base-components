6c92b101befcb83c3148708565057631e8947633
Jermaine <jermaine@Jermaines-Mac-mini.local>
2020-07-24 16:28:12 -0700

    Setup announcement bar

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,17 +1,28 @@
 <script>
-import Vue from 'vue';
-import { ShopBaseComponentsSample } from '@/entry';
+import Vue from 'vue'
+import { ShopBaseComponentsSample } from '@/entry'
+import { AnnouncementBar } from '@/entry'
 
 export default Vue.extend({
   name: 'ServeDev',
   components: {
     ShopBaseComponentsSample,
+    AnnouncementBar
   }
-});
+})
 </script>
 
 <template>
   <div id="app">
     <shop-base-components-sample />
+    <announcement-bar
+      :enabled="true"
+      backgroundColor="#220e89"
+      textColor="#FFF"
+      :slides="[
+        { url: '', text: 'Hello world' },
+        { url: '', text: 'Lorem ipsum dolor sit amet' }
+      ]"
+    />
   </div>
 </template>
---
+++ src/lib-components/AnnouncementBar.vue
@@ -0,0 +1,80 @@
+<template>
+  <div :style="cssProps" v-if="enabled" class="announcement-bar">
+    <span>
+      {{ slides[this.currentSlide].text }}
+    </span>
+  </div>
+</template>
+
+<script lang="">
+export default {
+  name: 'AnnouncementBar',
+  data() {
+    return {
+      currentSlide: 0
+    }
+  },
+  props: {
+    enabled: {
+      type: Boolean,
+      default: () => {
+        return false
+      }
+    },
+    textColor: {
+      type: String,
+      // TODO validate hex color
+      default: () => {
+        return ''
+      }
+    },
+    backgroundColor: {
+      // TODO validate hex color
+      type: String,
+      default: () => {
+        return ''
+      }
+    },
+    slides: {
+      // TODO validate array of objects
+      type: Array,
+      default: () => {
+        return []
+      }
+    },
+    speed: {
+      type: Number,
+      default: () => {
+        return 1000
+      }
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      setInterval(() => {
+        this.currentSlide === this.slides.length + 1
+          ? (this.currentSlide = 0)
+          : (this.currentSlide += 1)
+      }, this.speed)
+    }
+  },
+  computed: {
+    cssProps() {
+      return {
+        '--text-color': this.textColor,
+        '--background-color': this.backgroundColor
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.announcement-bar {
+  background: var(--background-color);
+  color: var(--text-color);
+}
+</style>

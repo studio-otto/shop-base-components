f8ba5cf4f234ea05319ccbe6283563e389537f72
Jermaine <jermaine@Jermaines-Mac-mini.local>
2020-07-27 12:29:24 -0700

    remove background color test for now, add testing-library utils

--- src/lib-components/__tests__/announcement-bar.spec.js
+++ src/lib-components/__tests__/announcement-bar.spec.js
@@ -1,6 +1,8 @@
-import { render } from '@testing-library/vue'
+import { render, cleanup } from '@testing-library/vue'
 import '@testing-library/jest-dom/extend-expect'
 import AnnouncementBar from '../announcement-bar.vue'
+
+afterEach(cleanup)
 
 test('renders', async () => {
   const { getByTestId } = render(AnnouncementBar, {
@@ -19,20 +21,6 @@ test('renders given text', async () => {
   const { getByText } = render(AnnouncementBar, {
     props: {
       enabled: true,
-      slides: [{ url: 'https://testing.com', text: 'Hello World' }]
-    }
-  })
-
-  const bar = getByText('Hello World')
-
-  expect(bar).toBeInTheDocument()
-})
-
-test('renders given background color', async () => {
-  const { getByText } = render(AnnouncementBar, {
-    props: {
-      enabled: true,
-      backgroundColor: `#2d942a`,
       slides: [{ url: 'https://testing.com', text: 'Hello World' }]
     }
   })

e3b7329227b09be4678ecb29c4a9f875c2ca94ff
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2020-08-21 11:38:07 -0700

    Setup vue glide on cpc

--- src/lib-components/collection-product-card.vue
+++ src/lib-components/collection-product-card.vue
@@ -1,12 +1,27 @@
 <template lang="">
   <div class="cpc" data-testid="cpc">
-    <div class="cpc__image">
-      <responsive-image
-        v-if="images.length > 0"
-        :lazySrcSet="displayImage"
-        :altText="title + 'image'"
-        :isFeatured="!!isUsingFeatured"
-      />
+    <div
+      class="cpc__image"
+      @mouseenter="isHovered = true"
+      @mouseleave="isHovered = false"
+    >
+      <transition name="fade">
+        <responsive-image
+          v-if="!isHovered"
+          :lazySrcSet="displayImage"
+          :altText="`${title} image`"
+          :isFeatured="!!isUsingFeatured"
+        />
+        <vue-glide v-else :perView="1" :startAt="1">
+          <vue-glide-slide v-for="(image, index) in images" :key="index">
+            <responsive-image
+              :lazySrcSet="image.src"
+              :altText="`${title} image`"
+              :isFeatured="!!isUsingFeatured"
+            />
+          </vue-glide-slide>
+        </vue-glide>
+      </transition>
     </div>
     <div class="cpc__details">
       <div class="cpc__details-title">
@@ -23,77 +38,82 @@
 </template>
 
 <script lang="">
-import ResponsiveImage from './responsive-image.vue'
+import ResponsiveImage from "./responsive-image.vue";
+import { Glide, GlideSlide } from "vue-glide-js";
+// May need to remove this to make fade transition?
+import "vue-glide-js/dist/vue-glide.css";
 
 export default {
   name: `CollectionProductCard`,
   components: {
-    ResponsiveImage
+    ResponsiveImage,
+    [Glide.name]: Glide,
+    [GlideSlide.name]: GlideSlide
   },
   data() {
     return {
       isHovered: false
-    }
+    };
   },
   props: {
     title: {
       type: String,
       default: () => {
-        return ``
+        return ``;
       }
     },
     price: {
       type: Number,
       default: () => {
-        return 0
+        return 0;
       }
     },
     isUsingFeatured: {
       type: Boolean,
       default: () => {
-        return false
+        return false;
       }
     },
     featuredImage: {
       type: Object,
       default: () => {
-        return null
+        return null;
       }
     },
     compareAtPrice: {
       type: Number,
       default: () => {
-        return null
+        return null;
       }
     },
     images: {
       type: Array,
       default: () => {
-        return []
+        return [];
       }
     }
   },
   computed: {
     isSaleItem() {
-      return this.compareAtPrice && this.compareAtPrice < this.price
+      return this.compareAtPrice && this.compareAtPrice < this.price;
     },
     displayImage() {
       if (this.featuredImage && this.isUsingFeatured) {
-        return this.featuredImage
+        return this.featuredImage;
       } else {
         return this.isUsingStorefront
           ? this.images
             ? this.images[0]
-            : ''
+            : ""
           : this.images
           ? this.images[0].src
-          : ''
+          : "";
       }
     }
   },
   methods: {
     toggleHover() {
-      this.isHovered = !this.isHovered
+      this.isHovered = !this.isHovered;
     }
   }
   // data () { return {} },
@@ -128,5 +148,24 @@ export default {
   // extends: {},
   // delimiters: [ '{{', '}}' ],
   // functional: false
-}
+};
 </script>
+
+<style lang="scss">
+.cpc {
+  ul {
+    margin: 0;
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+  position: absolute;
+}
+</style>

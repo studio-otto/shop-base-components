bf4b848e1fd4e8358a1bd215c12b668686f765ba
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2020-09-23 14:26:57 -0600

    Add actions buttons to lineItem component

--- src/lib-components/cart-tray/line-item.vue
+++ src/lib-components/cart-tray/line-item.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="cart-tray__line-item">
     <ResponsiveImage :lazySrcSet="lineItemImage" :altText="lineItem.title" />
+    <div class="cart-tray__line-item-counter">
+      <span @click="updateLineItem">-</span>
+      <span>{{ lineItem.quantity }}</span>
+      <span @click="updateLineItem">+</span>
+    </div>
+    <span @click="removeLineItem">Remove</span>
   </div>
 </template>
 

eff4e6612b40231213c3b7305e878ecb8d147e8a
Frankie Bushell <fbushell@gmail.com>
2021-05-06 18:16:12 -0700

    Added gsap transitions to modal component

--- src/lib-components/modal.vue
+++ src/lib-components/modal.vue
@@ -1,26 +1,27 @@
 <template>
   <div
-    :class="[`modal`, open ? `modal-visible` : `modal-hidden`]"
+    :class="['modal', open ? 'pointer-events-all' : 'pointer-events-none']"
     role="dialog"
     :aria-modal="open"
     data-testid="modal"
   >
     <div
+      ref="mask"
       class="modal__mask"
-      @click="$emit('close')"
-      @keydown.enter="$emit('close')"
-      tabindex="0"
       aria-label="Close modal"
+      tabindex="0"
       data-testid="modal-mask"
+      @click="closeModal"
+      @keydown.enter="closeModal"
     ></div>
 
-    <div class="modal__inner">
+    <div ref="content" class="modal__inner">
       <div
         class="modal__close"
-        @click="$emit('close')"
-        @keydown.enter="$emit('close')"
         aria-label="Close modal"
         tabindex="0"
+        @click="closeModal"
+        @keydown.enter="closeModal"
       >
         <svg
           width="11px"
@@ -34,7 +35,6 @@
             <g fill-rule="nonzero" fill="#000000">
               <path
                 d="M5.5,4.79289322 L1.64517136,0.938064583 L0.938064583,1.64517136 L4.79289322,5.5 L0.938064583,9.35482864 L1.64517136,10.0619354 L5.5,6.20710678 L9.39328103,10.1003878 L10.1003878,9.39328103 L6.20710678,5.5 L10.1003878,1.60671897 L9.39328103,0.899612188 L5.5,4.79289322 Z"
-                id="Combined-Shape"
               ></path>
             </g>
           </g>
@@ -48,21 +48,72 @@
   </div>
 </template>
 
-<script lang="">
+<script>
+import gsap from 'gsap'
+
 export default {
   name: 'Modal',
   props: {
     open: {
       type: Boolean,
-      default: () => {
-        return false
-      }
+      default: false
     },
+
+    useFade: {
+      type: Boolean,
+      default: true
+    },
+
     title: {
       type: String,
-      default: () => {
-        return ``
+      default: ''
+    }
+  },
+
+  watch: {
+    open(newVal, oldVal) {
+      if (newVal) {
+        this.openModal()
+      } else {
+        this.closeModal()
       }
+    }
+  },
+
+  methods: {
+    openModal() {
+      gsap.to(this.$refs.mask, {
+        opacity: 1,
+        duration: 0.4,
+        ease: 'power4.out'
+      })
+
+      gsap.to(this.$refs.content, {
+        opacity: 1,
+        y: 0,
+        delay: 0.4,
+        duration: 0.4,
+        ease: 'power4.out'
+      })
+    },
+
+    closeModal() {
+      gsap.to(this.$refs.content, {
+        opacity: 0,
+        duration: 0.4,
+        y: 10,
+        ease: 'power4.out'
+      })
+
+      gsap.to(this.$refs.mask, {
+        opacity: 0,
+        duration: 0.4,
+        delay: 0.2,
+        ease: 'power4.out',
+        onComplete: () => {
+          this.$emit('close')
+        }
+      })
     }
   }
 }
@@ -79,14 +130,6 @@ export default {
   align-items: center;
   justify-content: center;
 
-  &.modal-visible {
-    display: flex;
-  }
-
-  &.modal-hidden {
-    display: none;
-  }
-
   .modal__mask {
     position: fixed;
     width: 100%;
@@ -94,6 +137,7 @@ export default {
     background: rgba(255, 255, 255, 0.9);
     cursor: pointer;
     z-index: 0;
+    opacity: 0;
   }
 
   .modal__inner {
@@ -102,6 +146,8 @@ export default {
     z-index: 2;
     background: white;
     padding: 1rem;
+    opacity: 0;
+    transform: translateY(10px);
     box-shadow: 0 30px 130px rgba(0, 0, 0, 0.26);
   }
 
